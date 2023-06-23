4961f907bd2834d91452bee4497893d55978f5f9
jonatanare <jonatanare.dev@gmail.com>
2023-06-23 09:45:00 -0500

    add user endpoint and schema architecture

--- components/user/model.js
+++ components/user/model.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 
 const mySchema = new Schema({
     name: String
+},{
+    timestamps: true
 })
 
-const model = mongoose.model('Message', mySchema)
+const model = mongoose.model('User', mySchema)
 module.exports = model
